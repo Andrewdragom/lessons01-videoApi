4e8f2b6a909b73e65c77e3a57edea5945c868131
Andrewdragom <andreasdragomirov@yandex.com>
2024-11-09 23:48:38 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import {SETTINGS} from "./setting";
 import {videosRouter} from "./routes/videos-router";
-
+import cors from 'cors'
 export const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use(SETTINGS.PATH.VIDEOS, videosRouter);
 
 app.get('/', (req, res) => {
